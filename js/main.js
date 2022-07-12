17c6abeed22d1810160a098ba80ddabb8bc1ba0a
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-13 01:31:08 +0200

    desafio clase 6

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+const profiles = [];
+
+function addProfile(profile) {
+    profiles.push(profile);
+    alert("Profile added for user "+profile.name)
+
+}
+
+let profileName = prompt("Enter you name")
+
+addProfile({id: 1, name: profileName})
